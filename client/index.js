a67cebfad7a188ddbda4e45d52126e26cbddb339
Benjamin Esham <benjamin@esham.io>
2017-09-06 14:09:31 -0700

    Use a fetch polyfill to make the client-side code way smaller

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
+require('whatwg-fetch');
 const leaflet = require('leaflet');
-const request = require('request-promise-native');
 
 let map;
 let marker_layer;
@@ -7,6 +7,16 @@ let marker_layer;
 // Returns, in an array, all the parts of "text" that look like HTTP or HTTPS URLs.
 function extract_urls(text) {
 	return text.match(/(\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig);
+}
+
+function check_fetch_status(response) {
+	if (response.status >= 200 && response.status < 300) {
+		return response;
+	} else {
+		const error = new Error(response.statusText);
+		error.response = response;
+		throw error;
+	}
 }
 
 function initialize_map() {
@@ -34,22 +44,20 @@ function map_these_urls_handler() {
 	const entered_text = document.getElementById('urls').value;
 
 	const urls = extract_urls(entered_text);
-	const request_promises = urls.map(url =>
-		request.get({
-			uri: document.location.origin + '/extract_locations',
-			qs: {
-				url: url,
-			},
-			gzip: true,
-			json: true
-		}).then(function(places) {
-			places.forEach(place =>
-				leaflet.marker([place.latitude, place.longitude])
-					.bindPopup(place.title)
-					.addTo(marker_layer));
-		}).catch(function(reason) {
-			console.error(reason);
-		}));
+	const request_promises = urls.map(function(url) {
+		const request_uri = document.location.origin + '/extract_locations?url=' + encodeURIComponent(url);
+		return fetch(request_uri)
+			.then(check_fetch_status)
+			.then(response => response.json())
+			.then(function(places) {
+				places.forEach(place =>
+					leaflet.marker([place.latitude, place.longitude])
+						.bindPopup(place.title)
+						.addTo(marker_layer));
+			}).catch(function(reason) {
+				console.error(reason);
+			});
+	});
 
 	Promise.all(request_promises).then(function() {
 		map.fitBounds(marker_layer.getBounds());
