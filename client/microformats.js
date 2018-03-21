36c187f23cbe7146751d806826df0fd75c60861a
Benjamin Esham <benjamin@esham.io>
2018-03-21 11:11:44 -0700

    Add support for finding nested microformats (fixes #1)

--- client/microformats.js
+++ client/microformats.js
@@ -1,8 +1,42 @@
 const microformats = require('microformat-node');
 
+if (!Object.values) {
+	require('object.values').shim();
+}
+
+/* Given an array of Microformats2 objects, recursively looks for "h-adr" and
+ * "h-geo" items, returning them in a flat array.
+ */
+function find_locations(objects) {
+	let results = [];
+
+	for (const object of objects) {
+		if (object.type && (object.type.includes('h-adr') || object.type.includes('h-geo'))) {
+			results.push(object);
+
+			// There could be locations nested inside this one (in
+			// object.properties or object.children), but that doesn't really
+			// make sense semantically, so we just ignore them.
+			continue;
+		}
+
+		if (object.properties) {
+			for (const property_array of Object.values(object.properties)) {
+				results = results.concat(find_locations(property_array));
+			}
+		}
+
+		if (object.children) {
+			results = results.concat(find_locations(object.children));
+		}
+	}
+
+	return results;
+}
+
 function find_locatable_microformats(html) {
 	return microformats.getAsync({html: html})
-		.then(objects => objects.items.filter(item => item.type.includes('h-adr') || item.type.includes('h-geo')));
+		.then(result => find_locations(result.items));
 }
 
 function geotag_places(places, containing_url, geocode) {
--- client_test/microformats.js
+++ client_test/microformats.js
@@ -37,6 +37,42 @@ test('extracting one non-nested h-geo', function(t) {
 		});
 });
 
+test('extracting one nested h-adr', function(t) {
+	const html = `
+		<div class="h-entry"><p class="p-location h-adr">
+			<span class="p-region">Texas</span>,
+			<span class="p-country-name">United States</span>
+		</p></div>`;
+
+	return microformats.find_locatable_microformats(html)
+		.then(function(items) {
+			t.equal(items.length, 1, 'one microformat was found');
+
+			const item = items[0];
+			t.deepEqual(item.type, ['h-adr'], 'the microformat was an h-adr');
+			t.deepEqual(item.properties.region, ['Texas'], 'the region was parsed correctly');
+			t.deepEqual(item.properties['country-name'], ['United States'], 'the country was parsed correctly');
+		});
+});
+
+test('extracting one nested h-geo', function(t) {
+	const html = `
+		<div class="h-entry"><span class="p-location h-geo">
+			<data class="p-latitude" value="45.4340"/>
+			<data class="p-longitude" value="12.3388"/>
+		</span>`;
+
+	return microformats.find_locatable_microformats(html)
+		.then(function(items) {
+			t.equal(items.length, 1, 'one microformat was found');
+
+			const item = items[0];
+			t.deepEqual(item.type, ['h-geo'], 'the microformat was an h-geo');
+			t.deepEqual(item.properties.latitude, ['45.4340'], 'the latitude was parsed correctly');
+			t.deepEqual(item.properties.longitude, ['12.3388'], 'the longitude was parsed correctly');
+		});
+});
+
 test('extracting one non-nested h-geo with extraneous elements', function(t) {
 	const html = `
 		<div class="h-geo">
@@ -80,5 +116,118 @@ test('extracting two non-nested h-geos', function(t) {
 			t.deepEqual(two.type, ['h-geo'], 'the second microformat was an h-geo');
 			t.deepEqual(two.properties.latitude, ['28.3675'], 'the second latitude was parsed correctly');
 			t.deepEqual(two.properties.longitude, ['-81.548333'], 'the second longitude was parsed correctly');
+		});
+});
+
+test('extracting two nested h-geos', function(t) {
+	const html = `
+		<div class="h-entry">
+			<span class="p-location h-geo">
+				<data class="p-latitude" value="45.4340"/>
+				<data class="p-longitude" value="12.3388"/>
+			</span>
+			<span class="p-location h-geo">
+				<data class="p-latitude" value="28.3675"/>
+				<data class="p-longitude" value="-81.548333"/>
+			</span>
+		</div>`;
+
+	return microformats.find_locatable_microformats(html)
+		.then(function(items) {
+			t.equal(items.length, 2, 'two microformats were found');
+
+			const one = items[0];
+			t.deepEqual(one.type, ['h-geo'], 'the first microformat was an h-geo');
+			t.deepEqual(one.properties.latitude, ['45.4340'], 'the first latitude was parsed correctly');
+			t.deepEqual(one.properties.longitude, ['12.3388'], 'the first longitude was parsed correctly');
+
+			const two = items[1];
+			t.deepEqual(two.type, ['h-geo'], 'the second microformat was an h-geo');
+			t.deepEqual(two.properties.latitude, ['28.3675'], 'the second latitude was parsed correctly');
+			t.deepEqual(two.properties.longitude, ['-81.548333'], 'the second longitude was parsed correctly');
+		});
+});
+
+test('extracting two h-geos at different levels of nesting (one is a subproperty)', function(t) {
+	const html = `
+		<div class="h-entry">
+			<div class="p-author h-card">
+				<span class="p-location h-geo">
+					<data class="p-latitude" value="45.4340"/>
+					<data class="p-longitude" value="12.3388"/>
+				</span>
+			</div>
+			<span class="p-location h-geo">
+				<data class="p-latitude" value="28.3675"/>
+				<data class="p-longitude" value="-81.548333"/>
+			</span>
+		</div>`;
+
+	return microformats.find_locatable_microformats(html)
+		.then(function(items) {
+			t.equal(items.length, 2, 'two microformats were found');
+
+			const one = items[0];
+			t.deepEqual(one.type, ['h-geo'], 'the first microformat was an h-geo');
+			t.deepEqual(one.properties.latitude, ['45.4340'], 'the first latitude was parsed correctly');
+			t.deepEqual(one.properties.longitude, ['12.3388'], 'the first longitude was parsed correctly');
+
+			const two = items[1];
+			t.deepEqual(two.type, ['h-geo'], 'the second microformat was an h-geo');
+			t.deepEqual(two.properties.latitude, ['28.3675'], 'the second latitude was parsed correctly');
+			t.deepEqual(two.properties.longitude, ['-81.548333'], 'the second longitude was parsed correctly');
+		});
+});
+
+test('extracting two h-geos at different levels of nesting (one is a child)', function(t) {
+	const html = `
+		<div class="h-entry">
+			<div class="h-card">
+				<span class="p-location h-geo">
+					<data class="p-latitude" value="45.4340"/>
+					<data class="p-longitude" value="12.3388"/>
+				</span>
+			</div>
+			<span class="p-location h-geo">
+				<data class="p-latitude" value="28.3675"/>
+				<data class="p-longitude" value="-81.548333"/>
+			</span>
+		</div>`;
+
+	return microformats.find_locatable_microformats(html)
+		.then(function(items) {
+			t.equal(items.length, 2, 'two microformats were found');
+
+			const one = items[0];
+			t.deepEqual(one.type, ['h-geo'], 'the first microformat was an h-geo');
+			t.deepEqual(one.properties.latitude, ['28.3675'], 'the first latitude was parsed correctly');
+			t.deepEqual(one.properties.longitude, ['-81.548333'], 'the first longitude was parsed correctly');
+
+			const two = items[1];
+			t.deepEqual(two.type, ['h-geo'], 'the second microformat was an h-geo');
+			t.deepEqual(two.properties.latitude, ['45.4340'], 'the second latitude was parsed correctly');
+			t.deepEqual(two.properties.longitude, ['12.3388'], 'the second longitude was parsed correctly');
+		});
+});
+
+test('an h-geo nested inside another one is ignored', function(t) {
+	const html = `
+		<div class="h-geo">
+			<span class="h-geo">
+				<data class="p-latitude" value="45.4340"/>
+				<data class="p-longitude" value="12.3388"/>
+			</span>
+			<data class="p-latitude" value="28.3675"/>
+			<data class="p-longitude" value="-81.548333"/>
+		</div>`;
+
+	return microformats.find_locatable_microformats(html)
+		.then(function(items) {
+			t.equal(items.length, 1, 'one microformat was found');
+
+			const one = items[0];
+			t.deepEqual(one.type, ['h-geo'], 'the microformat was an h-geo');
+			t.deepEqual(one.properties.latitude, ['28.3675'], 'the latitude was parsed correctly');
+			t.deepEqual(one.properties.longitude, ['-81.548333'], 'the longitude was parsed correctly');
 		});
 });
